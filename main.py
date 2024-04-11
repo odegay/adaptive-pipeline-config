c2381028fc1bb1ec192ab96d4c15c9e1f5c7cd6e
odegay <dolegan@gmail.com>
2024-04-11 14:47:20 -0400

    added service-functions instead of the local ones

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 import json
 from google.cloud import pubsub_v1
 import requests
+from adpipsvcfuncs import publish_to_pubsub
+from adpipwfwconst import MSG_TYPE
+from adpipwfwconst import PIPELINE_TOPICS as TOPICS
 import logging
 
 logger = logging.getLogger(__name__)
@@ -15,17 +18,17 @@ def helper_function_1():
 def helper_function_2():
     # Replace with your helper function's logic
     print("Helper function 2 executed")
-def publish_to_pubsub(topic_name, project_id, data):
-    """Publishes a message to a Google Cloud Pub/Sub topic."""
-    # Publish the message to Pub/Sub
-      # Publish the message to Pub/Sub (with dynamic project_id)
-    publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path(project_id, topic_name)
-    data = json.dumps(data).encode("utf-8")
-    future = publisher.publish(topic_path, data)
-    print(future.result()) 
-    # Data must be a bytestring
-    # Publish the message, the result is a future that provides details on the delivery
+# def publish_to_pubsub(topic_name, project_id, data):
+#     """Publishes a message to a Google Cloud Pub/Sub topic."""
+#     # Publish the message to Pub/Sub
+#       # Publish the message to Pub/Sub (with dynamic project_id)
+#     publisher = pubsub_v1.PublisherClient()
+#     topic_path = publisher.topic_path(project_id, topic_name)
+#     data = json.dumps(data).encode("utf-8")
+#     future = publisher.publish(topic_path, data)
+#     print(future.result()) 
+#     # Data must be a bytestring
+#     # Publish the message, the result is a future that provides details on the delivery
 
 def validate_message(event, context):
     """Background Cloud Function to be triggered by Pub/Sub.
@@ -64,7 +67,7 @@ def adatptive_pipeline_generate_config(event, context):
             # ... add more data if needed 
         }
         topic_name = "adaptive-pipeline-workflow-topic"
-        publish_to_pubsub(topic_name, project_id, message_data)
+        publish_to_pubsub(topic_name, message_data)
         print(f"Successfully published a message to {topic_name} and project_id {project_id}")
         return f"Successfully published a message to {topic_name} and project_id {project_id}"
     else:
